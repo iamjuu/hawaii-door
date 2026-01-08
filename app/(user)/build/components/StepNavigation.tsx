133638971f1f03cf844aa9cb6b1f881ea2e452fe
muees <abdulmueesp123@gmail.com>
2026-01-08 15:55:35 +0530

    updates

--- app/(user)/build/components/StepNavigation.tsx
+++ app/(user)/build/components/StepNavigation.tsx
@@ -16,16 +16,16 @@ const StepNavigation = ({ onBack, onNext, showBack, percentage }: StepNavigation
         {showBack && (
           <button
             onClick={onBack}
-            className="flex items-center gap-[10px] px-4 h-[37px] w-[149.97px] bg-white border-2 border-[#EAEAEA] rounded-[10px] text-black hover:text-gray-900 transition-colors shadow-md font-roboto"
+            className="flex items-center gap-[10px] px-4 h-[37px] w-[143.97px] bg-white border-2 border-[#EAEAEA] rounded-[10px] text-black hover:text-gray-900 transition-colors shadow-md font-roboto"
           >
-            <GoChevronLeft className="text-black text-base" />
-            <span className="text-[18px] font-roboto text-black">GO BACK</span>
+            <GoChevronLeft className="text-black text-[20px]" />
+            <span className="text-[16px] font-roboto text-black">GO BACK</span>
           </button>
         )}
       </div>
 
       {/* Center: Progress */}
-      <div className="flex items-center flex-1 justify-center gap-4 ml-[40px] ">
+      <div className="flex items-center flex-1 justify-center gap-4 ml-[29px] ">
         <div 
           className="h-[35px] w-full max-w-[800px] rounded-[12px] relative overflow-hidden flex items-center justify-center shadow-inner"
           style={{ backgroundColor: '#F6F6F6' }}
@@ -47,10 +47,10 @@ const StepNavigation = ({ onBack, onNext, showBack, percentage }: StepNavigation
       <div className="w-32 flex justify-end">
         <button
           onClick={onNext}
-          className="flex items-center gap-[10px] px-4 h-[37px] bg-[#FF6E4A] text-white rounded-l-[10px] hover:opacity-90 transition-opacity font-roboto"
+          className="flex items-center gap-[7px] w-[110px] px-4 h-[37px] bg-[#FF6E4A] text-white rounded-l-[10px] hover:opacity-90 transition-opacity font-roboto"
         >
-          <span className="text-[18px] font-roboto">NEXT</span>
-          <GoChevronRight className="text-white text-base" />
+          <span className="text-[16px] font-roboto">NEXT</span>
+          <GoChevronRight className="text-white text-[20px]" />
         </button>
       </div>
     </div>
