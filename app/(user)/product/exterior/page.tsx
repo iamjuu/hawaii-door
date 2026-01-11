3b822c3dc1f98ec7b27c88d8c580314d3fd0342f
muees <abdulmueesp123@gmail.com>
2026-01-11 17:56:33 +0530

    updates

--- app/(user)/product/exterior/page.tsx
+++ app/(user)/product/exterior/page.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
+
 import Navbar from '@/components/user/Navbar'
 import Footer from '@/components/user/Footer'
-import { ProductFootericoncheck, ProductFootericonstar,ProductFooter,FooterSetting,FooterTool} from '@/public/assets';
+import { ProductFootericoncheck, ProductFootericonstar} from '@/public/assets';
 import HeroSection from '../components/herosection';
 import Houseimage from '../../../../public/assets/product/exterior/house.jpg';
 import Windowimage from '../../../../public/assets/product/exterior/window.jpg';
-import Image from 'next/image';
-import { MdOutlineArrowForward, MdOutlineArrowOutward } from 'react-icons/md';
+import FooterBanner from '../components/footerbanner';
 const ExteriorPage = () => {
   const bgImage = "/assets/product/exterior/exterior.png";
   const contant = "Exterior Doors";
@@ -123,112 +122,7 @@ const ExteriorPage = () => {
     </div>
   </div>
 </section>
-
-<div className="w-full px-5 md:px-15 mb-12 py-8 md:pt-12 md:pb-16 lg:pb-20 bg-white  ">
-      <div className="max-w-[1400px] mx-auto">
-        <div className="relative w-full min-h-[400px] md:min-h-[420px] bg-[#84684C] rounded-lg overflow-hidden">
-
-          {/* ================= LEFT CONTENT ================= */}
-          <div className="relative z-10 flex flex-col justify-center h-full px-8 md:px-12 lg:px-16 pt-12 md:py-16">
-            <div className="max-w-xl">
-              <h2 className="font-roboto ffont-semibold text-[26px] md:text-[46px] text-white leading-tight mb-6">
-              Create Your Custom<br /> 
-              Door
-              </h2>
-
-              <p className="font-roboto font-[400] text-[14px] md:text-[18px]  text-[#C6C6C6]  mb-8 md:w-[490px] ">
-              Explore doors designed for precision fit, with custom jambs and built to Hawaii Spec for lasting durability. Elevate your space with doors tailored to your style and needs.
-              </p>
-
-              {/* Button */}
-              <button className="group relative inline-flex items-center gap-3 overflow-hidden rounded-3xl bg-[#FF6E4A] text-[15px] md:text-lg font-roboto px-5 py-2  text-white">
-                {/* Hover overlay */}
-                <span className="absolute inset-0 bg-black origin-bottom scale-y-0 transition-transform duration-700 ease-[cubic-bezier(0.4,0,0.2,1)] group-hover:scale-y-100" />
-
-                {/* Button content */}
-                <span className="relative z-10 flex items-center gap-3 font-roboto">
-                Start Building Your Perfect Custom Door Now
-                  <span className="inline-flex items-center justify-center w-7 h-7 transition-all duration-500 rotate-0 translate-x-1.5 group-hover:rotate-45 group-hover:translate-x-0">
-                    <MdOutlineArrowOutward className="text-white text-2xl" />
-                  </span>
-                </span>
-              </button>
-            </div>
-          </div>
-
-          {/* ================= DESKTOP IMAGE ================= */}
-         {/* ================= DESKTOP IMAGE ================= */}
-<div className="absolute right-0 bottom-0 top-10 hidden md:block w-[50%] h-full pr-10">
-  <div className="relative w-full h-full">
-
-    {/* MAIN DOOR IMAGE */}
-    <Image
-      src={ProductFooter}
-      alt="Door styles"
-      fill
-      priority
-      className="object-contain object-bottom"
-    />
-
-    {/* SETTINGS ICON */}
-    <Image
-      src={FooterSetting}
-      alt="Settings icon"
-      width={130}
-      height={130}
-      className="absolute top-[34%] right-[23%] z-20"
-    />
-
-    {/* TOOL ICON */}
-    <Image
-      src={FooterTool}
-      alt="Tool icon"
-      width={120}
-      height={120}
-      className="absolute bottom-[10%] right-[55%] z-20 rotate-[-10deg]"
-    />
-
-  </div>
-</div>
-
-
-          {/* ================= MOBILE IMAGE ================= */}
-        {/* ================= MOBILE IMAGE ================= */}
-<div className="relative md:hidden w-full h-[260px] mt-8">
-
-{/* DOOR */}
-<Image
-  src={ProductFooter}
-  alt="Door styles"
-  fill
-  priority
-  className="object-contain object-bottom"
-/>
-
-{/* SETTINGS */}
-<Image
-  src={FooterSetting}
-  alt="Settings icon"
-  width={70}
-  height={70}
-  className="absolute top-23 right-20 z-20"
-/>
-
-{/* TOOL */}
-<Image
-  src={FooterTool}
-  alt="Tool icon"
-  width={80}
-  height={80}
-  className="absolute bottom-8 left-1/2 -translate-x-1/2 z-20"
-/>
-</div>
-
-
-        </div>
-      </div>
-    </div>
-
+<FooterBanner />
       <Footer />
       </div>
   )
