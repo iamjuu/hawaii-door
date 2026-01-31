7feccdcb23000b97c65a25b9f44c7190411bc20a
Nithin <nithin@autosmarts.co>
2026-02-01 00:01:14 +0530

    button size changed

--- app/(user)/home/components/Banner.tsx
+++ app/(user)/home/components/Banner.tsx
@@ -98,7 +98,7 @@ export default function BannerSection() {
   label="Build Your Door"
   icon={<MdOutlineArrowOutward className="text-2xl" />}
   hoverVariant="white"
-  className="cursor-pointer"
+  className="cursor-pointer !w-fit !h-auto !px-6 !py-3 !text-base md:!text-lg"
 />
                     </Link>
                   </div>
