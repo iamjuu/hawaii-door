2ed9cbb5ab260d477f46631a9f363cc4b45d9150
Nithin <nithin@autosmarts.co>
2026-04-16 22:24:52 +0530

    currenlyt user track is done

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,6 +1,16 @@
 import { ReactNode } from "react";
+import Script from "next/script";
 
 export default function UserLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Script
+        id="zoho-pagesense"
+        strategy="afterInteractive"
+        src="https://cdn.pagesense.io/js/903394376/36033de429fb468f997e68fe6c1b1c23.js"
+      />
+      {children}
+    </>
+  );
 }
 
